5acc7d63fee58c1c39beb43ce7c34c44985707b1
sanzamcmillian <g18s2658@ru.ac.za>
2025-03-24 19:44:14 +0200

    feat: create pytests

---
+++ tests/test_payments.py
@@ -0,0 +1,35 @@
+import pytest
+from app import app, db
+from storage.models import Payments
+
+@pytest.fixture
+def client():
+    """Set up a test client and initialize the database."""
+    app.config["TESTING"] = True
+    app.config["SQLACHEMY_DATABASE_URI"] = "sqlite://:memory"
+    with app.test_client() as client:
+        with app.app_context():
+            db.create_all()
+        yield client
+        with app.app_context():
+            db.drop_all()
+
+
+def test_create_payment(client):
+    """Test payment creation endpoint."""
+    response = client.post("/v1/payments", json={
+        "customer_name": "John Doe",
+        "customer_email": "john@example.com",
+        "amount": 50.00
+    })
+    assert response.status_code == 200
+    data = response.get_json()
+    assert  "message" in data
+    assert data["message"] == "Payment successful" or "Payment created"
+
+
+def test_create_payment_missing_fields(client):
+    """Test error when missing fields."""
+    response = client.post("/v1/payments", json={})
+    assert response.status_code == 400
+    assert "error" in data
